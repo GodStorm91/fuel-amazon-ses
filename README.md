e9ed6e57d44a9beb4f3136243ceeaea051d2d153
グエンカイン <khanh@in-g.jp>
2020-08-28 11:41:26 +0900

    Update package

--- README.md
+++ README.md
@@ -4,13 +4,17 @@ Adds basic support for Amazon's Simple Email Service to the FuelPHP 1.1 Email Dr
 
 # Install
 
-In the repository's root directory, run "git clone git://github.com/rob-mccann/fuel-amazon-ses.git fuel/packages/amazon-ses" (without quotes).
+In the repository's root directory, run "git clone git://github.com/GodStorm91/fuel-amazon-ses.git fuel/packages/amazon-ses" (without quotes).
+This packages support AWS Signature V4 ( with will be effective from 2020 October).
+If you are using V3 Signature ( can detect by the line below), please consider upgrade your package.
+
+```php
+$curl->set_header('X-Amzn-Authorization','AWS3-HTTPS AWSAccessKeyId='.\Config::get('ses.access_key').', Algorithm=HmacSHA256, Signature=' . $signature)
+```
 
 # Usage
 
-You can use this exactly the same way as you would use the default email protocols. You'll need to copy and paste the config file from /fuel/packages/amazon-ses/config/ses.php to /fuel/app/config/ses.php and set your API keys accordingly.
-
-Fork and pull request any useful changes you make.
+I just changed the signing algorithm, so the usage should stay the same with the original package.
 
 ```php
 Email::forge(array('driver' => 'ses'))
--- classes/email/driver/ses.php
+++ classes/email/driver/ses.php
@@ -1,19 +1,24 @@
 <?php
 
 /**
- * Amazon Email Delivery library for FuelPHP
+ * Amazon Email Delivery library for FuelPHP (Support AWS Signature V4)
  *
- * @package		Amazon
+ * @package		FuelPHP SES Driver ( base on Rob McCann's package)
  * @version		1.1
- * @author		Rob McCann (rob@robmccann.co.uk)
- * @link		http://github.com/unforeseen/fuel-amazon-ses
+ * @author		Khanh Nguyen (khanh@in-g.jp)
+ * @link		https://github.com/GodStorm91/fuel-amazon-ses
  * 
  */
 
-class Email_Driver_SES extends \Email_Driver {
-
+class Email_Driver_Ses extends \Email_Driver {
 	public $region = null;
+	public $service = 'ses';
+	public $credentialScope;
+
 	protected $debug = true;
+    const ISO8601_BASIC = 'Ymd\THis\Z';
+    const UNSIGNED_PAYLOAD = 'UNSIGNED-PAYLOAD';
+    const AMZ_CONTENT_SHA256_HEADER = 'X-Amz-Content-Sha256';
 	
 	public function __construct($config) 
 	{
@@ -21,8 +26,8 @@ class Email_Driver_SES extends \Email_Driver {
 		\Config::load('ses', true);
 		
 		$this->region = \Config::get('ses.region','us-east-1');
-	}
 
+	}
 	/**
 	 * Sends the email using the Amazon SES email delivery system
 	 * 
@@ -32,10 +37,11 @@ class Email_Driver_SES extends \Email_Driver {
 	{
 		$params = array(
 			'Action' => 'SendEmail',
+			'Version' => '2010-12-01',
 			'Source' => static::format_addresses(array($this->config['from'])),
 			'Message.Subject.Data' => $this->subject,
-			'Message.Body.Text.Data' => $this->alt_body,
-			'Message.Body.Html.Data' => $this->body
+			'Message.Body.Text.Data' => $this->body,
+			'Message.Body.Text.Charset' => $this->config['charset'],
 		);
 		
 		$i = 0;
@@ -65,36 +71,243 @@ class Email_Driver_SES extends \Email_Driver {
 			$params['ReplyToAddresses.member.'.($i+1)] = static::format_addresses(array($value));
 			++$i;
 		}	
-
-		$date = date(DATE_RSS);
-		$signature = $this->_sign_signature($date);
+		$date = date(self::ISO8601_BASIC);
+		$dateRss = date(DATE_RSS);
 		
 		$curl = \Request::forge('https://email.' . $this->region . '.amazonaws.com/', array(
 			'driver' => 'curl',
 			'method' => 'post'
-		))->set_header('Content-Type','application/x-www-form-urlencoded')
-			->set_header('X-Amzn-Authorization','AWS3-HTTPS AWSAccessKeyId='.\Config::get('ses.access_key').', Algorithm=HmacSHA256, Signature=' . $signature)
-			->set_header('Date', $date);
+			))
+			->set_header('Content-Type','application/x-www-form-urlencoded')
+			->set_header('date', $dateRss)
+			->set_header('host', 'email.' . $this->region . '.amazonaws.com')
+			->set_header('x-amz-date', $date);
+		$signature = $this->_sign_signature_v4($params);
+		$curl->set_header('Authorization', $signature);
+		$response = $curl->execute($params);
 		
-		$response = $curl->execute($params);		
 		
 		if (intval($response-> response()->status / 100) != 2) 
 		{
+			\Log::debug("Send mail errors " . json_encode($response->response()));
 			return false;
 		}
 		
+		\Log::debug("Send mail ok " . json_encode($response->response()));
 		return true;
 	}
-	
+
 	/**
-	 * Calculate signature
-	 * @param	string	date used in the header
-	 * @return	string 	RFC 2104-compliant HMAC hash
+	 * Sets the from address and name
+	 *
+	 * @param   string      $email  The from email address
+	 * @param   bool|string $name   The optional from name
+	 *
+	 * @return  $this
 	 */
-	private function _sign_signature($date)
+	public function from($email, $name = false)
 	{
-		$hash = hash_hmac('sha256', $date, \Config::get('ses.secret_key'), TRUE);
-		return base64_encode($hash);
+		$this->config['from']['email'] = (string) $email;
+		$this->config['from']['name']  = (is_string($name)) ? $name : false;
+
+		if ($this->config['from']['name'])
+		{
+			$this->config['from']['name'] = $this->encode_mimeheader((string) $this->config['from']['name']);
+		}
+
+		return $this;
 	}
+
+	/**
+	 * Sign a curl request V4
+	 *
+	 * @param   object      $curl
+	 * @param   object		$curl object
+	 *
+	 * @return  $this
+	 */
+	private function _sign_signature_v4($bodyParams)
+	{
+        $ldt = gmdate(self::ISO8601_BASIC);
+        $sdt = substr($ldt, 0, 8);
+        $parsed = [
+            'method'  => 'POST',
+            'path'    => '/',
+            'query'   => [],
+			'uri'     => 'https://email.' . $this->region . '.amazonaws.com/',
+            'headers' => [
+							'host' => ['email.' . $this->region . '.amazonaws.com'],
+							'x-amz-date' => [date(DATE_RSS)],
+							],
+            'body'    => $bodyParams,
+            'version' => 'HTTP/1.1'
+        ];
+		$cs = $this->createScope($sdt, $this->region, $this->service);
+		$this->credentialScope = $cs;
+		$payload = http_build_query($bodyParams);   
+
+        $context = $this->createContext($parsed, hash('sha256', $payload));
+		$toSign = $this->createStringToSign($ldt, $cs, $context['creq']);
+
+        $signingKey = $this->getSigningKey(
+            $sdt,
+            \Config::get('ses.region'),
+            'ses',
+            \Config::get('ses.secret_key')
+		);
+		
+		$signature = hash_hmac('sha256', $toSign, $signingKey);
+		$accessKey = \Config::get('ses.access_key');
+        $parsed['headers']['Authorization'] = [
+            "AWS4-HMAC-SHA256 "
+            . "Credential={$accessKey}/{$cs}, "
+            . "SignedHeaders=host;x-amz-date, Signature={$signature}"
+		];
+		
+		return implode("", $parsed['headers']['Authorization']);
+	}
+
+    /**
+     * @param array  $parsedRequest
+     * @param string $payload Hash of the request payload
+     * @return array Returns an array of context information
+     */
+    private function createContext($parsedRequest, $payload)
+    {
+        $blacklist = $this->getHeaderBlacklist();
+
+        // Normalize the path as required by SigV4
+        $canon = $parsedRequest['method'] . "\n"
+            . $this->createCanonicalizedPath($parsedRequest['path']) . "\n"
+            . $this->getCanonicalizedQuery($parsedRequest['query']) . "\n";
+
+        // Case-insensitively aggregate all of the headers.
+        $aggregate = [];
+        foreach ($parsedRequest['headers'] as $key => $values) {
+            $key = strtolower($key);
+            if (!isset($blacklist[$key])) {
+                foreach ($values as $v) {
+                    $aggregate[$key][] = $v;
+                }
+            }
+        }
+
+        ksort($aggregate);
+        $canonHeaders = [];
+
+		$canonHeaders[] = "host:" . 'email.' . $this->region . '.amazonaws.com';
+		$canonHeaders[] = "x-amz-date:" . date(self::ISO8601_BASIC);
+        $signedHeadersString = implode(';', array_keys($aggregate));
+        $canon .= implode("\n", $canonHeaders) . "\n\n"
+            . $signedHeadersString . "\n"
+            . $payload;
+
+        return ['creq' => $canon, 'headers' => $signedHeadersString];
+    }
+
+    /**
+     * @param array  $path
+     * @return string Returns string in canocicalized
+     */
+	protected function createCanonicalizedPath($path)
+    {
+        $doubleEncoded = rawurlencode(ltrim($path, '/'));
+
+        return '/' . str_replace('%2F', '/', $doubleEncoded);
+    }
+
+    private function createStringToSign($longDate, $credentialScope, $creq)
+    {
+        $hash = hash('sha256', $creq);
+
+        return "AWS4-HMAC-SHA256\n{$longDate}\n{$credentialScope}\n{$hash}";
+    }
+
+
+    private function createScope($shortDate, $region, $service)
+    {
+        return "$shortDate/$region/$service/aws4_request";
+    }
+
+	private function getSigningKey($shortDate, $region, $service, $secretKey)
+    {
+        $k = $shortDate . '_' . $region . '_' . $service . '_' . $secretKey;
+		$dateKey = hash_hmac(
+			'sha256',
+			$shortDate,
+			"AWS4{$secretKey}",
+			true
+		);
+		$regionKey = hash_hmac('sha256', $region, $dateKey, true);
+		$serviceKey = hash_hmac('sha256', $service, $regionKey, true);
+		$signingKey = hash_hmac(
+			'sha256',
+			'aws4_request',
+			$serviceKey,
+			true
+		);
+
+		return $signingKey;
+	}
+	
+    /**
+     * The following headers are not signed because signing these headers
+     * would potentially cause a signature mismatch when sending a request
+     * through a proxy or if modified at the HTTP client level.
+     *
+     * @return array
+     */
+    private function getHeaderBlacklist()
+    {
+        return [
+            'cache-control'         => true,
+            'content-type'          => true,
+            'content-length'        => true,
+            'expect'                => true,
+            'max-forwards'          => true,
+            'pragma'                => true,
+            'range'                 => true,
+            'te'                    => true,
+            'if-match'              => true,
+            'if-none-match'         => true,
+            'if-modified-since'     => true,
+            'if-unmodified-since'   => true,
+            'if-range'              => true,
+            'accept'                => true,
+            'authorization'         => true,
+            'proxy-authorization'   => true,
+            'from'                  => true,
+            'referer'               => true,
+            'user-agent'            => true,
+            'x-amzn-trace-id'       => true,
+            'aws-sdk-invocation-id' => true,
+            'aws-sdk-retry'         => true,
+        ];
+    }
+
+	private function getCanonicalizedQuery(array $query)
+    {
+        unset($query['X-Amz-Signature']);
+
+        if (!$query) {
+            return '';
+        }
+
+        $qs = '';
+        ksort($query);
+        foreach ($query as $k => $v) {
+            if (!is_array($v)) {
+                $qs .= rawurlencode($k) . '=' . rawurlencode($v) . '&';
+            } else {
+                sort($v);
+                foreach ($v as $value) {
+                    $qs .= rawurlencode($k) . '=' . rawurlencode($value) . '&';
+                }
+            }
+        }
+
+        return substr($qs, 0, -1);
+    }
+
 
 }
